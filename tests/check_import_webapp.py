60d60ed59f3c514d3093f749cf82c49c577eff4e
furqan5911 <furqanmalick2001@gmail.com>
2025-12-21 17:03:13 +0500

    Docs: README for frontend; ignore data, tests, migrations and supabase artifacts

--- tests/check_import_webapp.py
+++
@@ -1,9 +0,0 @@
-import traceback
-try:
-    import importlib
-    importlib.invalidate_caches()
-    import webapp_new
-    print('imported webapp_new OK')
-except Exception:
-    traceback.print_exc()
-    raise
